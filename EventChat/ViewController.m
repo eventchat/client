d99deef9c4b4b60841878b564f6e7a1d2bdd26f1
taojason <taojason@gmail.com>
2014-07-22 16:12:43 -0700

    complete login view

--- EventChat/ViewController.m
+++ EventChat/ViewController.m
@@ -9,11 +9,14 @@
 #import "ViewController.h"
 #import "ApiUtil.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UIView *dialogView;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIView *loadingView;
+@property (weak, nonatomic) IBOutlet UIImageView *emailImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
 @property (strong, nonatomic) NSDictionary *data;
 
 - (IBAction)loginButtonDidPress:(id)sender;
@@ -36,10 +39,39 @@
 
     // Change status bar
     [self setNeedsStatusBarAppearanceUpdate];
+    
+    // Set delegates
+    self.emailTextField.delegate = self;
+    self.passwordTextField.delegate = self;
+    
+    // Add listener for textfield change
+    [self.emailTextField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
 }
 
 -(UIStatusBarStyle) preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField {
+    // Highlight Email TextField
+    if ([textField isEqual:self.emailTextField]) {
+        NSLog(@"email text field is edited");
+        [self.emailTextField setBackground:[UIImage imageNamed:@"input-outline-active"]];
+        self.emailImageView.image = [UIImage imageNamed:@"icon-mail-active"];
+    } else {
+        [self.emailTextField setBackground:[UIImage imageNamed:@"input-outline"]];
+        self.emailImageView.image = [UIImage imageNamed:@"icon-mail"];
+    }
+    
+    // Highlight Password TextField
+    if ([textField isEqual:self.passwordTextField]) {
+        NSLog(@"password text field is edited");
+        [self.passwordTextField setBackground:[UIImage imageNamed:@"input-outline-active"]];
+        self.passwordImageView.image = [UIImage imageNamed:@"icon-password-active"];
+    } else {
+        [self.passwordTextField setBackground:[UIImage imageNamed:@"input-outline"]];
+        self.passwordImageView.image = [UIImage imageNamed:@"icon-password"];
+    }
 }
 
 - (void)doErrorMessage {
@@ -65,8 +97,18 @@
     } completion:^(BOOL finished) {}];
 }
 
+-(void)textFieldDidChange:(UITextField *) textField {
+    if (textField.text.length > 20) {
+        self.emailImageView.hidden = YES;
+    } else {
+        self.emailImageView.hidden = NO;
+    }
+}
+
 - (IBAction)loginButtonDidPress:(id)sender {
 
+    // hide loadingView
+    self.loadingView.hidden = NO;
     
     NSString *email = self.emailTextField.text;
     NSString *password = self.passwordTextField.text;
@@ -86,17 +128,19 @@
                                             double delayInSeconds = 1.0f;   // Just for debug
                                             dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
                                             dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-                                                
+
+                                                                                                self.loadingView.hidden = YES;
                                                 // Get response
                                                 self.data = json;
                                                 NSString *token = [self.data valueForKeyPath:@"access_token"];
                                                 NSLog(@"%@", self.data);
-                                                
+
                                                 // if logged in
                                                 if (token) {
                                                     // do something after logged in
                                                     NSLog(@"I am logged in!");
-                                                    
+                                                    // perform segue
+                                                    [self performSegueWithIdentifier:@"loginToHomeScene" sender:self];
                                                 } else {
                                                     [self doErrorMessage];
                                                 }
