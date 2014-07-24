53c81295a84af99292be40d99e4487497ba0f083
taojason <taojason@gmail.com>
2014-07-23 19:42:35 -0700

    added ui for My Events page, added files for custom EventCell

---
+++ EventChat/EventCell.h
@@ -0,0 +1,13 @@
+//
+//  EventCell.h
+//  EventChat
+//
+//  Created by Jianchen Tao on 7/23/14.
+//  Copyright (c) 2014 EventChat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventCell : UITableViewCell
+
+@end
