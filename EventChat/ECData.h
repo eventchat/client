910057498acaa14fa0255b2ede23f4628e17af9e
taojason <taojason@gmail.com>
2014-08-03 20:43:23 -0700

    add some getter methods for ECData

--- EventChat/ECData.h
+++ EventChat/ECData.h
@@ -9,5 +9,4 @@
 #import <Foundation/Foundation.h>
 
 @interface ECData : NSObject
-
 @end
--- EventChat/ECData.m
+++ EventChat/ECData.m
@@ -7,7 +7,48 @@
 //
 
 #import "ECData.h"
+#import "Message.h"
+
+NSMutableDictionary *conversationsDict;
+NSMutableDictionary *eventsDict;
+NSMutableDictionary *friendsDict;
 
 @implementation ECData
+
+- (ECData *) init {
+    self = [super init];
+    conversationsDict = [[NSMutableDictionary alloc] init];
+    eventsDict = [[NSMutableDictionary alloc] init];
+    friendsDict = [[NSMutableDictionary alloc] init];
+    return self;
+}
+
+- (NSMutableDictionary *) getConversationsDict {
+    return conversationsDict;
+}
+
+
+- (void) addConversationWithResponderId: (NSString *) responderId WithMessage: (Message *) message {
+    
+    
+    [conversationsDict setObject:message forKey:responderId];
+}
+
+- (NSMutableDictionary *) getEventsDict {
+    return eventsDict;
+}
+
+- (void) addEvent {
+    
+}
+
+- (NSMutableDictionary *) getFriendsDict {
+    return friendsDict;
+}
+
+- (void) addFriend {
+    
+}
+
 
 @end
