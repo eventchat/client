8bacedd113aae59a7f0c1afb5c7dfd982a93f487
taojason <taojason@gmail.com>
2014-08-03 21:04:07 -0700

    delete redundant comment.h ; create Converstation data structure

---
+++ EventChat/Conversation.h
@@ -0,0 +1,13 @@
+//
+//  Conversation.h
+//  EventChat
+//
+//  Created by Jianchen Tao on 8/3/14.
+//  Copyright (c) 2014 EventChat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Conversation : NSObject
+
+@end
