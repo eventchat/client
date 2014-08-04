f0e2a8ec2cc91422d7ea30863c4008a4c918f34c
Lyman Cao <caoyifanlyman@gmail.com>
2014-08-03 21:05:44 -0700

    add class attendee

---
+++ EventChat/Attendee.h
@@ -0,0 +1,13 @@
+//
+//  Attendee.h
+//  EventChat
+//
+//  Created by Lyman Cao on 8/3/14.
+//  Copyright (c) 2014 EventChat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Attendee : NSObject
+
+@end
